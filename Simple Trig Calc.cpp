189a46689d528613d88dcdb7d6e9674f45077a82
xSh4dowXSniPerx <deerkiller1996@gmail.com>
2015-05-13 12:26:29 -0400

    Added comments

--- Simple Trig Calc.cpp
+++ Simple Trig Calc.cpp
@@ -1,17 +1,28 @@
 // Example program
+
+// Necessary for system function
+#include <cstdlib>
+
+// Necessary for function such as cout and cin, ect.
 #include <iostream>
+
+// For strings
 #include <string>
+
+// Allows use of complex math functions
 #include <cmath>
 
 using namespace std;
 int main(){
  
+ // Variabels
  double opp, adj, hyp, sinsol, cossol, tansol;
  char sin, cos, tan;
  
  cout << "What are you trying to find? (sin, cos, tan)" << endl;
  cin >>sin >>cos >>tan;
  
+ // If sin then compute opp/hyp, if cos then compute adj/hyp, and if tan then compute opp/adj.
  if(sin){
      cout << "Input values opp and hyp." << endl;
      cin >>opp >>hyp;
@@ -29,5 +40,7 @@ int main(){
      cout << "Answer: " << tansol << endl;
  }
  
+ // Bad way to pause functions. Os depency and slow.
+ system("pause");
  return 0;
 }
