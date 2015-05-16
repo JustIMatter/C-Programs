daae039a3bd30c0db77d98c8add9749f4a304db8
xSh4dowXSniPerx <deerkiller1996@gmail.com>
2015-05-16 19:42:58 -0400

    Completed FinalGradeCalculator and runs as should

--- FinalGradeCalculator.cpp
+++ FinalGradeCalculator.cpp
@@ -1,7 +1,7 @@
 /* Complex Grading Program
 * Involves multiple grades and uses weighting of grades
 * as well as total averages of weighting to calculate
-finall grade. */
+final grade. */
 
 #include <cstdlib>
 #include <iostream>
@@ -12,38 +12,78 @@ using namespace std;
 
 int main(){
 
-	double minor, quiz, major;
-	int minorG, quizG, majorG;
+	double minorW, quizW, majorW, minorG, quizG, majorG, minorGSum, quizGSum, majorGSum, totalGSum;
+	int minorGrades, quizGrades, majorGrades;
 
 	minorG = 0;
+	quizG = 0;
+	majorG = 0;
+	minorGSum = 0;
+	quizGSum = 0;
+	majorGSum = 0;
 
 	cout << "Final Grade Calculator" << endl;
 
 	cout << "Input minor grade weight percent." << endl;
-	cin >>minor;
+	cin >>minorW;
 
 	cout << "Input quiz grade weight percent." << endl;
-	cin >>quiz;
+	cin >>quizW;
 
 	cout << "Input major grade weight percent." << endl;
-	cin >>major;
+	cin >>majorW;
+
+	cout << "Enter number of minor grades." << endl;
+	cin >>minorGrades;
+
+	cout << "Enter number of quiz grades." << endl;
+	cin >>quizGrades;
+
+	cout << "Enter number of major grades." << endl;
+	cin >>majorGrades;
 
 	// Three grade categories
-	minor = minor/100;
-	quiz = quiz/100;
-	major = major/100;
+	minorW = minorW / 100;
+	quizW = quizW / 100;
+	majorW = majorW / 100;
 
 
-	for(int i = 1; i <=10; i++){
-		cout << "Input a minor grade. (Max=10)" << endl;
+	for(int i = 1; i <= minorGrades; i++){
+		cout << "Input a minor grade." << endl;
 		cin >>minorG;
 
-
-		int *minorGA = new int[minorG];
-
-		minorG+= minorGA[minorG];
-		cout << "Currently: " << i << " Grade(s)." <<endl;
+		minorGSum += minorG;
+		cout << "Currently: " << i << " minor grade(s)." <<endl;
 	}
-	cout << "Minor Sum: " << minorG << endl;
+	minorG = minorGSum * minorW;
+
+	for(int i = 1; i <= quizGrades; i++){
+		cout << "Input a quiz grade." << endl;
+		cin >>quizG;
+
+		quizGSum += quizG;
+		cout << "Currently: " << i << " quiz grade(s)." <<endl;
+	}
+	quizG = quizGSum * quizW;
+
+	for(int i = 1; i <= majorGrades; i++){
+		cout << "Input a major grade." << endl;
+		cin >>majorG;
+
+		majorGSum += majorG;
+		cout << "Currently: " << i << " major grade(s)." <<endl;
+	}
+	majorG = majorGSum * majorW;
+
+	minorG = minorG / minorGrades;
+	quizG = quizG / quizGrades;
+	majorG = majorG / majorGrades;
+	totalGSum = minorG + quizG + majorG;
+
+	cout << "Minor Grade: " << minorG << endl;
+	cout << "Quize Grade: " << quizG << endl;
+	cout << "Major Grade: " << majorG << endl;
+	cout << "Total Grade: " << totalGSum << endl;
+
 	return 0;
 }
