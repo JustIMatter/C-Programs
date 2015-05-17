b83f5c8f1e6782c0e7bec4db587db98c7eb2ee95
xSh4dowXSniPerx <deerkiller1996@gmail.com>
2015-05-17 00:19:46 -0400

    Added AgeCalculator.cpp

---
+++ AgeCalculator.cpp
@@ -0,0 +1,26 @@
+// Age Calculator
+/* Tell age in years,
+* days, months, hours,
+minutes, and seconds */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	
+	int dayBirth, monthBirth, yearBirth, dayCurrent, monthCurrent, yearCurrent; 
+	cout << "Enter todays date. (Day Month Year)" << endl;
+	cin >>dayCurrent >>monthCurrent >>yearCurrent;
+
+	cout << "Enter your birth date. (Day Month Year)" << endl;
+	cin >>dayBirth >>monthBirth >>yearBirth;
+
+	
+
+
+	return 0;
+}
